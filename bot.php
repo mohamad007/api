9705a2592672d98c04270745b365e3324bd2c146
mohamad007 <mohamad007@users.noreply.github.com>
2016-12-15 20:29:03 -0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,114 +1,54 @@
-<?php 
-$token = '276874700:AAFWzQdCIuXY8GzaSEgxa8M8QdkMltwsxwk';
-// read incoming info and grab the chatID 
-$json = file_get_contents('php://input');
-$telegram = urldecode ($json);
-$telegram = str_replace ('jason=','',$telegram); //Just for Teletter.net
-$results = json_decode($telegram); 
-
-
-
-		
-// send reply
- // $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id=80853440&text='.$json;
- // file_get_contents($url);
-
-
-
-
-
-
-
-
-$message = $results->message;
-$text = $message->text;
-$chat = $message->chat;
-$user_id = $chat->id;
-
-// if ($text == 'salam') {
-	// $message = 'سلام';
-// } else if ($text == 'چطوری') {
-	// $message = 'خوبم';
-// } else {
-	// $message = 'نمیدونم';
-// }
-
-// // send reply
-// $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$message.'&reply_to_message_id=224';
-// file_get_contents($url);
-
-
-
-
-
-
-$message = 'websima Academy';
-$message = urldecode($message);
-// websima_sendmessage ($user_id,$message,$token);
-// //Send Text Message 
-// function websima_sendmessage ($user_id,$message,$token) {
-	// $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$message;
-	// $update = file_get_contents($url);
-// }
-
-
-
-
-
-
-
-
-// $keyboard = array(
-					// array( 'ورود به سایت'),
-					// array( 'amin'),
-					// array ('وبسیما','آکادمی','test'),
-				// );
-
-// $keyboard = array(
-					// array( array('text'=>'شماره تماس','request_contact'=>true)),
-				// );
-// websima_keyboardmessage ($user_id,$message,$token,$keyboard);
-
-// //Send Text Message With Inline Keyboard
-// function websima_keyboardmessage ($user_id,$message,$token,$keyboard) {
-	// $url = 'https://api.telegram.org/bot'.$token.'/sendMessage';
-	// $replyMarkup = array(
-				// 'keyboard' => $keyboard,
-				// 'resize_keyboard' => true
-			// );
-	// $encodedMarkup = json_encode($replyMarkup);
-	// $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$message.'&reply_markup='.$encodedMarkup;
-	// $update = file_get_contents($url);
-// }
-
-
-// $keyboard = array(
-					// array( 'ورود به سایت'),
-					// array ('وبسیما','آکادمی'),
-				// );
-
-$keyboard = array(
-					array( 
-					array('text'=>'شماره تماس','url'=>'http://websima.com'),
-					array('text'=>'اطلاعات','callback_data'=>'1')
-					),
-				);
-websima_inlinekeyboardmessage ($user_id,$message,$token,$keyboard);
-
-//Send Text Message With Inline Keyboard
-function websima_inlinekeyboardmessage ($user_id,$message,$token,$keyboard) {
-	$url = 'https://api.telegram.org/bot'.$token.'/sendMessage';
-	$replyMarkup = array(
-				'inline_keyboard' => $keyboard
-			);
-	$encodedMarkup = json_encode($replyMarkup);
-	$url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$message.'&reply_markup='.$encodedMarkup;
-	$update = file_get_contents($url);
+<?php
+ob_start();
+define('API_KEY','286108480:AAEVjtelIdebiEfDuP8eM3aAfIkjCxDC0Pg');
+$admin =  "193930120";
+$update = json_decode(file_get_contents('php://input'));
+$from_id = $update->message->from->id;
+$chat_id = $update->message->chat->id;
+$text = $update->message->text;
+$jock = file_get_contents("http://roonx.com/api.php?roonx=time");
+function roonx($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
 }
-
-
-
-
-
-
-?>
+if(preg_match('/^\/([Ss]tart)/',$text)){
+roonx('sendMessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"سلام به ربات زمان خوش اومدی:)
+	دستور زیر رو برای دیدن زمان بفرست
+	/time",
+    'parse_mode'=>'html'
+  ]);
+}elseif(preg_match('/^\/([Tt]ime)/',$text)){
+roonx('sendMessage',[
+    'chat_id'=>$chat_id,
+    'text'=>$jock,
+    'parse_mode'=>'html'
+  ]);
+}elseif(preg_match('/^\/([Ss]tats)/',$text) and $from_id == $admin){
+    $user = file_get_contents('Member.txt');
+    $member_id = explode("\n",$user);
+    $member_count = count($member_id) -1;
+    roonx('sendMessage',[
+      'chat_id'=>$chat_id,
+      'text'=>"تعداد کل اعضا: $member_count",
+      'parse_mode'=>'HTML'
+    ]);
+}
+$user = file_get_contents('Member.txt');
+    $members = explode("\n",$user);
+    if (!in_array($chat_id,$members)){
+      $add_user = file_get_contents('Member.txt');
+      $add_user .= $chat_id."\n";
+     file_put_contents('Member.txt',$add_user); 
+    }
+	?>
