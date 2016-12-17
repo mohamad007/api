75a79d786a6fced6bbd6cea6a2da4b23f7429add
mohamad007 <mohamad007@users.noreply.github.com>
2016-12-16 20:29:09 -0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-define('API_KEY','293548592:AAHrU-cRJM-U1xSeMXymZYPoOm91DDn0yM8');
+define('API_KEY','290575108:AAGXkVKHlVcxw3MovveXtgnNetR0nwoewiY');
 $admin =  "193930120";
 $update = json_decode(file_get_contents('php://input'));
 $from_id = $update->message->from->id;
@@ -19,36 +19,12 @@ function roonx($method,$datas=[]){
         return json_decode($res);
     }
 }
-if(preg_match('/^\/([Ss]tart)/',$text)){
-roonx('sendMessage',[
-    'chat_id'=>$chat_id,
-    'text'=>"سلام به ربات تاریخ و زمان خوش اومدی:)
-	دستور زیر رو برای دیدن زمان وتارخ بفرستید
-	/td",
-    'parse_mode'=>'html'
-  ]);
-}elseif{$txt = urlencode($text);
+if{$txt = urlencode($text);
 $time = file_get_contents("https://irapi.ir/simsimi/api2.php?text=$txt&lang=fa");
 roonx('sendMessage',[
     'chat_id'=>$chat_id,
-    'text'=>".$time.",
+    'text'=>$time,
     'parse_mode'=>'html'
   ]);
-}elseif(preg_match('/^\/([Ss]tats)/',$text) and $from_id == $admin){
-    $user = file_get_contents('Member.txt');
-    $member_id = explode("\n",$user);
-    $member_count = count($member_id) -1;
-    roonx('sendMessage',[
-      'chat_id'=>$chat_id,
-      'text'=>"تعداد کل اعضا: $member_count",
-      'parse_mode'=>'HTML'
-    ]);
 }
-$user = file_get_contents('Member.txt');
-    $members = explode("\n",$user);
-    if (!in_array($chat_id,$members)){
-      $add_user = file_get_contents('Member.txt');
-      $add_user .= $chat_id."\n";
-     file_put_contents('Member.txt',$add_user); 
-    }
 	?>
