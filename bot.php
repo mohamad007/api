6ec0a5300f1b809453da11331b3e9679a01a76ba
mohamad007 <mohamad007@users.noreply.github.com>
2016-12-22 01:45:40 -0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,12 +1,7 @@
 <?php
-ob_start();
-define('API_KEY','290575108:AAHXCw_JgMTBMpfACQOWzeGxB4XDUMwqMSo');
-$admin =  "193930120";
-$update = json_decode(file_get_contents('php://input'));
-$from_id = $update->message->from->id;
-$chat_id = $update->message->chat->id;
-$text = $update->message->text;
-function roonx($method,$datas=[]){
+define('API_KEY','322735835:AAF65PDyiv8ICeMtbUYB21CE9zM0SSMXrh8');
+//token
+function makereq($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,$url);
@@ -19,37 +14,72 @@ function roonx($method,$datas=[]){
         return json_decode($res);
     }
 }
-if(preg_match('/^\/([Ss]tart)/',$text)){
-roonx('sendMessage',[
-    'chat_id'=>$chat_id,
-    'text'=>"سلام به ربات تاریخ و زمان خوش اومدی:)
-  دستور زیر رو برای دیدن زمان وتارخ بفرستید
-  /td",
-    'parse_mode'=>'html'
-  ]);
-}elseif(preg_match('/^\/([Ss]tats)/',$text) and $from_id == $admin){
-    $user = file_get_contents('Member.txt');
-    $member_id = explode("\n",$user);
-    $member_count = count($member_id) -1;
-    roonx('sendMessage',[
-      'chat_id'=>$chat_id,
-      'text'=>"تعداد کل اعضا: $member_count",
-      'parse_mode'=>'HTML'
-    ]);
-} else{
-$txt = urlencode($text);
-    $time = file_get_contents("https://irapi.ir/simsimi/api2.php?text=$txt&lang=fa");
-roonx('sendMessage',[
-    'chat_id'=>$chat_id,
-    'text'=>$time,
-    'parse_mode'=>'html'
-  ]);
+//mrphp
+$update = json_decode(file_get_contents('php://input'));
+var_dump($update);
+$chat_id = $update->message->chat->id;
+$message_id = $update->message->message_id;
+$from_id = $update->message->from->id;
+$name = $update->message->from->first_name;
+$username = $update->message->from->username;
+$textmessage = isset($update->message->text)?$update->message->text:'';
+$rpto = $update->message->reply_to_message->forward_from->id;
+$forward = $update->message->forward_from;
+$txtmsg = $update->message->text;
+$Dev = 193930120;
+//mrphp
+function SendMessage($ChatId, $TextMsg)
+{
+ makereq('sendMessage',[
+'chat_id'=>$ChatId,
+'text'=>$TextMsg,
+'parse_mode'=>"MarkDown"
+]);
 }
-$user = file_get_contents('Member.txt');
-    $members = explode("\n",$user);
-    if (!in_array($chat_id,$members)){
-      $add_user = file_get_contents('Member.txt');
-      $add_user .= $chat_id."\n";
-     file_put_contents('Member.txt',$add_user); 
-    }
+//mrphp
+$inch = file_get_contents("https://api.telegram.org/bot".API_KEY."/getChatMember?chat_id=@MrPhPTeam&user_id=".$from_id);
+	
+	if (strpos($inch , '"status":"left"') !== false ) {
+SendMessage($chat_id,"سلام $name 
+براي استفاده از ربات باید در کانال زیر عضوبشی :
+@MrPhPTeam");
+}
+if($textmessage == '/start')
+var_dump(makereq('sendMessage',[
+        'chat_id'=>$update->message->chat->id,
+        'text'=>"سلام $name \n\nخوش اومدی به ربات آقای پی اچ پی ",
+        'parse_mode'=>'MarkDown',
+          	'reply_markup'=>json_encode([
+            	'keyboard'=>[
+                [
+                   ['text'=>"درباره ما"],['text'=>"امکانات"]
+                ],
+                 [
+                   ['text'=>"قوانین"],['text'=>"ارسال نظر"]
+                ]
+                
+            	],
+            	'resize_keyboard'=>true
+       		])
+    		]));
+}   
+elseif ($textmessage == 'درباره ما') {
+var_dump(makereq('sendMessage',[
+        'chat_id'=>$update->message->chat->id,
+        'text'=>"<code>تیم ربات سازی اقای پی اچ پی در سال 95-96 کار خودرا در زمینه ساخت ربات تلگرام (ای پی ای )شروع کرد ودر این زمینه تا به الان موفق بوده است.
+كانال ما : @MrPhPTeam
+برنامه نويس : @MrPhPSupport
+این ربات توسط زبان برنامه نویسی پی اچ پی کدنویسی شده است.</code>
+",
+	'parse_mode'=>'MarkDown',
+        'reply_markup'=>json_encode([
+            'inline_keyboard'=>[
+                [
+                    ['text'=>"رفتن به کانال",'url'=>"https://telegram.me/MrPhPTeam"],
+                    ['text'=>"توسعه دهنده",'url'=>"https://telegram.me/MrPhPSupprt"]
+                ]
+            ]
+        ])
+    ]));
+}
   ?>
